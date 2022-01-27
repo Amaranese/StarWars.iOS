4713d2bc8a07347b74ccff9533a65b1111d9fc43
Amaranese <32201849+Amaranese@users.noreply.github.com>
2022-01-27 10:50:32 +0100

    .

---
+++ Example/StarWars/StarWars.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for StarWars.
+FOUNDATION_EXPORT double StarWarsVersionNumber;
+
+//! Project version string for StarWars.
+FOUNDATION_EXPORT const unsigned char StarWarsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StarWars/PublicHeader.h>
+
+
